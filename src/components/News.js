a8eedbbeb22ad38fb4045bab460b35128efdc8fa
Dobopriya1379 <papai7909@gmail.com>
2021-09-21 12:54:26 +0530

    Top Loading Bar added

--- src/components/News.js
+++ src/components/News.js
@@ -15,44 +15,18 @@ function News(props) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    async function fetchNews() {
+    const updateNews = async ()=>{
+        props.setProgress(10);
         let url = `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&apiKey=483d4544dfc244c69d9a2cc3805bbfe1&page=1&pageSize=${props.pageSize}`;
-        setLoading(true)
+        setLoading(true);
         let data = await fetch(url);
+        props.setProgress(30);
         let parseData = await data.json()
+        props.setProgress(70);
         setArticles(parseData.articles);
         setLoading(false)
         setTotalresults(parseData.totalResults)
-    }
-
-    const prevPage = async () => {
-        console.log("Going to previous page")
-
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&apiKey=483d4544dfc244c69d9a2cc3805bbfe1&page=${page - 1}&pageSize=${props.pageSize}`;
-        setLoading(true)
-        let data = await fetch(url);
-        let parseData = await data.json()
-        setArticles(parseData.articles);
-        setLoading(false)
-        setPage(page - 1)
-        setTotalresults(parseData.totalResults)
-    }
-
-    const nextPage = async () => {
-        console.log("Going to next page")
-
-        if (page + 1 > Math.ceil(totalresults / 15)) {
-
-        } else {
-            let url = `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&apiKey=483d4544dfc244c69d9a2cc3805bbfe1&page=${page + 1}&pageSize=${props.pageSize}`;
-            setLoading(true)
-            let data = await fetch(url);
-            let parseData = await data.json()
-            setArticles(parseData.articles);
-            setLoading(false)
-            setPage(page + 1)
-            setTotalresults(parseData.totalResults)
-        }
+        props.setProgress(100);
     }
 
     const fetchMoreData = async () => {
@@ -67,7 +41,7 @@ function News(props) {
     }
 
     useEffect(() => {
-        fetchNews();
+        updateNews();
     }, [])
 
     return (
@@ -87,10 +61,6 @@ function News(props) {
                                 <NewsItems title={element.title ? element.title.slice(0, 48) : ""} desc={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} url={element.url} DateAndTime={element.publishedAt} Author={element.author} />
                             </div>
                         })}
-                    </div>
-                    <div className="container d-flex justify-content-between m-4 p-4">
-                        <button disabled={page <= 1} type="button" className="btn btn-dark" onClick={prevPage}>Previous</button>
-                        <button disabled={page + 1 > Math.ceil(totalresults / 15)} type="button" className="btn btn-dark" onClick={nextPage}>Next</button>
                     </div>
                 </div>
             </InfiniteScroll>
